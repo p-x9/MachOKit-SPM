6548ad892131b5604b0080d8999d0b80f954b9a1
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-01-13 06:04:41 +0900

    version 0.7.0

--- Package.swift
+++ Package.swift
@@ -7,17 +7,27 @@ let package = Package(
     products: [
         .library(
             name: "MachOKit",
-            targets: ["MachOKitBin", "_MachOKitSPM"]
+            targets: ["MachOKitBin", "MachOKitCBin", "_MachOKitSPM"]
         ),
     ],
     targets: [
         .binaryTarget(
             name: "MachOKitBin",
-            url: "https://github.com/p-x9/MachOKit/releases/download/0.6.0/MachOKit.xcframework.zip",
-            checksum: "8b181cfe3a1189a6db2204f62b7089d0bd4171e1ff42dbb6315b39b179f57094"
+            url: "https://github.com/p-x9/MachOKit/releases/download/0.7.0/MachOKit.xcframework.zip",
+            checksum: "198276c0068882661a4612ebf2c38bdf0ff87425987a0d2d2c190eb1063ad867"
+        ),
+        .binaryTarget(
+            name: "MachOKitCBin",
+            url: "https://github.com/p-x9/MachOKit/releases/download/0.7.0/MachOKitC.xcframework.zip",
+            checksum: "12c2cc3a8d15e559c4c7fd3e70e2ea37806a9963cc72f8ae68be2c9f5988e872"
         ),
         .target(
-            name: "_MachOKitSPM"
+            name: "_MachOKitSPM",
+            dependencies: ["MachOKitBin"]
+        ),
+        .testTarget(
+            name: "MachOKitSPMTests",
+            dependencies: ["MachOKitBin"]
         )
     ]
 )
--- Tests/MachOKitSPMTests/MachOKitSPMTests.swift
+++ Tests/MachOKitSPMTests/MachOKitSPMTests.swift
@@ -1,3 +1,4 @@
 import XCTest
+import MachOKit
 
 final class MachOKitSPMTests: XCTestCase {}
