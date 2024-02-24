9bc67e4da1026e2139e0baf71215ea0197d3e354
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-24 19:33:36 +0900

    add test case

--- Tests/MachOKitSPMTests/MachOKitSPMTests.swift
+++ Tests/MachOKitSPMTests/MachOKitSPMTests.swift
@@ -1,4 +1,9 @@
 import XCTest
 import MachOKit
 
-final class MachOKitSPMTests: XCTestCase {}
+final class MachOKitSPMTests: XCTestCase {
+    func test() {
+        let machO = MachOImage(name: "MachOKitSPMTests")!
+        print(machO)
+    }
+}
