cfcb8b38b46a544979e0dc0cbc38db46d31a05f6
GitHub Action <action@github.com>
2025-09-18 16:07:24 +0000

    version 0.39.0

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MachOKitBin",
-            url: "https://github.com/p-x9/MachOKit/releases/download/0.38.0/MachOKit.xcframework.zip",
-            checksum: "f55681c83002dedb277be61c511846de93f855ad4054d04742ea396bacf18655"
+            url: "https://github.com/p-x9/MachOKit/releases/download/0.39.0/MachOKit.xcframework.zip",
+            checksum: "bb3ee18b8e54b00342828cf59fdf1fc11c29ad6c0a699d6d3d5de7ac41852c3f"
         ),
         .binaryTarget(
             name: "MachOKitCBin",
-            url: "https://github.com/p-x9/MachOKit/releases/download/0.38.0/MachOKitC.xcframework.zip",
-            checksum: "218618d381e08955009c0bc752d83debebc1147ff086cbd3626623b0b8abd0ce"
+            url: "https://github.com/p-x9/MachOKit/releases/download/0.39.0/MachOKitC.xcframework.zip",
+            checksum: "52186e508cfa1229dad5f00486e454d27c3d8b4691761427796e51ce64468cd3"
         ),
         .target(
             name: "_MachOKitSPM",
